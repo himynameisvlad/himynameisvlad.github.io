6d732348c6abf7b14bb623cb07ed0e15e478d81c
himynameisvlad <klgmfff@gmail.com>
2014-01-21 09:19:10 -0800

    Update js.js

--- js.js
+++ js.js
@@ -13,9 +13,9 @@
 			}
 			)
 			$("#about-cl").on("click",function(){
-				$(".about").animate({opacity:"1"},1000).show(1000).stop()
+				$(".about").stop().animate({opacity:"1"},1000).show()
 			});
 			$("#skills").on("click",function(){
-				$(".about").animate({opacity:"0"},1000).hide(1000).stop()
+				$(".about").stop().animate({opacity:"0"},1000).hide()
 			})
 		})
