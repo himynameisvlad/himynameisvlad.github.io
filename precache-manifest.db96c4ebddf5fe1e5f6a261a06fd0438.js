88bd0b39c84d91a12fa013912a2317fc5029adfb
Vlad Mironov <vladmironov@Vlads-MacBook-Pro.local>
2019-05-05 20:00:24 +0400

    Added funbox build

---
+++ precache-manifest.db96c4ebddf5fe1e5f6a261a06fd0438.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645d083e8fe22246c37f24c5c013d0b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13026ba5f6dc6596981b",
+    "url": "/static/css/main.d7c0753a.chunk.css"
+  },
+  {
+    "revision": "9368d303b8d0a0be3503",
+    "url": "/static/js/2.adc1f70e.chunk.js"
+  },
+  {
+    "revision": "13026ba5f6dc6596981b",
+    "url": "/static/js/main.d1e1f937.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
